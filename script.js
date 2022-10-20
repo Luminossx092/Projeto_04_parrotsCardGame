691e0370152f9c0c9656a1170163638f38f3f27a
Luminossx092 <hehayase@gmail.com>
2022-10-20 11:18:59 -0300

    versao que pede numero de cartas pro jogador

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function virar(elemento){
+SetupJogo();
+function SetupJogo() {
+    let cartasJogadorEscolheu = Number(prompt("Com quantas cartas quer jogar?"));
+    let SeJogadorTiverEscolherDeNovo = (cartasJogadorEscolheu < 4 || cartasJogadorEscolheu > 14 || (cartasJogadorEscolheu % 2 == 1));
+    while (SeJogadorTiverEscolherDeNovo) {
+        cartasJogadorEscolheu = Number(prompt("Com quantas cartas quer jogar?"));
+        SeJogadorTiverEscolherDeNovo = (cartasJogadorEscolheu < 4 || cartasJogadorEscolheu > 14 || (cartasJogadorEscolheu % 2 == 1));
+    }
+}
+function virar(elemento) {
     elemento.classList.toggle("virada");
-  }
+}
