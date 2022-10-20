f4e93625b737887449b8a5c2f9df624a5fc6ca37
Luminossx092 <hehayase@gmail.com>
2022-10-20 12:06:16 -0300

    versao com cartas aleatorias viradas pra baixo

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-SetupJogo();
+let baralho = document.querySelector("ul");
+let ultimaCartaSelecionada;
+const parrots = ["bobrossparrot",
+    "explodyparrot",
+    "fiestaparrot",
+    "metalparrot",
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot"]
+//SetupJogo();
 function SetupJogo() {
     let cartasJogadorEscolheu = Number(prompt("Com quantas cartas quer jogar?"));
     let SeJogadorTiverEscolherDeNovo = (cartasJogadorEscolheu < 4 || cartasJogadorEscolheu > 14 || (cartasJogadorEscolheu % 2 == 1));
@@ -6,7 +15,31 @@ function SetupJogo() {
         cartasJogadorEscolheu = Number(prompt("Com quantas cartas quer jogar?"));
         SeJogadorTiverEscolherDeNovo = (cartasJogadorEscolheu < 4 || cartasJogadorEscolheu > 14 || (cartasJogadorEscolheu % 2 == 1));
     }
+    CriarCartas(cartasJogadorEscolheu);
+}
+
+function CriarCartas(numeroCartas) {
+    parrots.sort(comparador);
+    for (let i = 0; i < numeroCartas; i++) {
+        baralho.innerHTML += `<li onclick="virar(this)" class="Carta ${parrots[Math.floor(i/2)]}">
+        <div class="FaceCarta FrenteCarta">
+            <img src="./Images/${parrots[Math.floor(i/2)]}.gif">
+        </div>
+        <div class="FaceCarta VersoCarta">
+            <img src="./Images/back.png">
+        </div>
+        </li>`
+    }
+}
+
+
+function comparador() {
+    return Math.random() - 0.5;
 }
 function virar(elemento) {
+    if(ultimaCartaSelecionada != undefined){
+        
+    }
     elemento.classList.toggle("virada");
+
 }
